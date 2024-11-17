9ff34dd1adc3f329ba1650febc78e80205dcd3ac
roshilel <aronmaung@gmail.com>
2024-11-16 20:40:22 -0800

    Fixed Enemy Movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -15,8 +15,6 @@ public class EnemyMovement : MonoBehaviour
     MovePaths path;
 
     float t = 0;
-    float amplitude = 10f; // how wide move; larger # = wider
-    float period = 1.0f; //how fast move side-to-side; larger # = faster
 
     // Start is called before the first frame update
     void Awake()
@@ -37,14 +35,15 @@ public class EnemyMovement : MonoBehaviour
             }
             else if (path == MovePaths.SideToSide)
             {
-                rb.velocity = new Vector2(-1f, amplitude * Sin(t * period));
-                t += 0.01f;
+                rb.velocity = new Vector2(0.0f, 10.0f * Cos(t * 0.5f));
+                rb.velocity += new Vector2(-0.8f, 0.0f);
+                t += 0.1f;
             }
             else if (path == MovePaths.LooptyLoop)
             {
-                rb.velocity = new Vector2(10 * Cos(t * 5f), 10 * Sin(t * 5f));
+                rb.velocity = new Vector2(10.0f * Cos(t * 0.5f), 10.0f * Sin(t * 0.5f));
                 rb.velocity += new Vector2(-0.8f, 0.0f);
-                t += 0.01f;
+                t += 0.1f;
             }
         }
         
@@ -55,7 +54,7 @@ public class EnemyMovement : MonoBehaviour
         if (collision.gameObject.GetComponent<EnemyMovement>() == null)
         {
             collided = true;
-            rb.velocity = Vector2.Reflect(rb.velocity, Vector2.up);
+            rb.velocity = Vector2.Reflect(rb.velocity, Vector2.right);
         }
 
     }
