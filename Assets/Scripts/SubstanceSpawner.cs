d06148f9928256a8daa7d0297da877087d466e36
roshilel <aronmaung@gmail.com>
2024-11-17 22:26:45 -0800

    Updated Spawner Scripts

---
+++ Assets/Scripts/SubstanceSpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class SubstanceSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private float minSpawnTime;
+
+    [SerializeField]
+    private float maxSpawnTime;
+
+    private float timeUntilNextSpawn;
+
+    void Awake()
+    {
+        setTimeUntilNextSpawn();
+    }
+
+    void Update()
+    {
+        timeUntilNextSpawn -= Time.deltaTime;
+
+        if (timeUntilNextSpawn <= 0)
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+            setTimeUntilNextSpawn();
+        }
+    }
+
+    private void setTimeUntilNextSpawn()
+    {
+        timeUntilNextSpawn = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+}
